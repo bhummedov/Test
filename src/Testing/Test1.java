ac000ad877736a222013ccfcf3d27b1d40f5d6e0
unknown <user@JFMT2X2.ttu.edu>
2020-04-23 19:12:34 -0500

    Initial commit

---
+++ src/Testing/Test1.java
@@ -0,0 +1,33 @@
+package Testing;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Test1 {
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
+        Scanner scan = new Scanner(System.in);
+        ArrayList<Integer> temp = new ArrayList<>();
+        int number = 1;
+        for (int i = 0; i < 5; i++) {
+            arr.add(new ArrayList<>());
+            number = 1;
+            while (number != 0) {
+                System.out.print("Enter number");
+                number = scan.nextInt();
+                if (number != 0) {
+                    temp.add(number);
+                    //System.out.println(temp);
+                }
+            }
+            arr.get(i).addAll(temp);
+            temp.clear();
+        }
+            for (ArrayList<Integer> row : arr) {
+                for (int num : row) {
+                    System.out.print(num + "\t");
+                }
+                System.out.println();
+            }
+            System.out.println("Please enter your query");
+        }
+    }
